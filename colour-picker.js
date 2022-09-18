58fafac859e760b90accc0015d038ec05858702e
LauraK0 <laurakeating@live.co.uk>
2022-09-18 12:41:23 +0100

    js file overhauled
    code uses object orientated programme

--- colour-picker.js
+++ colour-picker.js
@@ -1,41 +1,106 @@
-function createHexCode() {
+//create colour object
+
+class Colour {
+	constructor(hex, element) {
+		this.hex = hex;
+		this.element = element;
+		this.locked = false;
+	}
+
+	setHex(hex) {
+		this.hex = hex;
+		this.element.style.backgroundColor = hex;
+		this.element.querySelector(".hex-code").innerHTML = hex; //Sets paragraphs with hex-code class as the generated hex code
+	}
+
+	setLocked(locked) {
+		this.locked = locked;
+
+		if (locked) {
+			this.element
+				.querySelector(".lock-button").classList.add("is-locked");
+
+			this.element
+				.querySelector("i").classList = "fa-solid fa-lock";
+		} else {
+			this.element
+				.querySelector(".lock-button").classList.remove("is-locked");
+
+			this.element
+				.querySelector("i").classList = "fa-solid fa-unlock";
+		}
+    document.activeElement.blur();
+	}
+
+	toggleLocked() {
+		this.setLocked(!this.locked);
+	}
+
+	generateHex() {
+		if (this.locked) {
+			return
+		}
+
     const newHexCode = '#' + Math.floor(Math.random() * (0xffffff + 1))
     .toString(16)
     .padStart(6, '0') 
-    return newHexCode.toUpperCase();
+    this.setHex(newHexCode);
+	}
+
+  copyToClipboard() {
+    const hexCodeTitle = this.element.querySelector(".hex-code").innerHTML;
+    console.log(hexCodeTitle)
+
+    navigator.clipboard.writeText(hexCodeTitle);   // Copy the text inside the text field
+    alert("Copied the text: " + hexCodeTitle);  // Alert the copied text
+
+    this.element.classList.add("copied");
+    setTimeout(() => {
+      //This variable removes the class copied from the element after a duration 1000ms
+      this.element.classList.remove("copied");
+    }, 1000);
+  }
 }
 
-function generateColourPalette(){
-    const colourSwatch = document.querySelectorAll(".colour-swatch");
-    const colourSwatchHexCode = document.querySelectorAll(".hex-code")
-    for (let i = 0; i < colourSwatch.length; i++) {
-        const randomColour = createHexCode();
-        if (colourSwatch[i].dataset.status === "unlocked") {
-        colourSwatch[i].style.background = randomColour;
-        colourSwatchHexCode[i].textContent = randomColour;
-        colourSwatch[i].dataset.hexcode = `${randomColour}`;
-        }
-    }
+//end colour object
+
+
+
+const colour_elements = document.querySelectorAll('.colour-palette .colour-swatch');
+
+const colours = [];
+
+for (let i = 0; i < colour_elements.length; i++) {
+	const colour_element = colour_elements[i];
+
+	const hexCodeTitle = colour_element.querySelector(".hex-code");
+	const lock_toggle = colour_element.querySelector(".lock-button");
+	const copy_btn = colour_element.querySelector(".copy-icon");
+
+	const hex = hexCodeTitle.value;
+	const colour = new Colour(hex, colour_element);
+
+	hexCodeTitle.addEventListener('.hex-code', (e) => colour.setHex(e.target.value));
+	lock_toggle.addEventListener('click', () => colour.toggleLocked());
+	copy_btn.addEventListener('click', () => colour.copyToClipboard());
+
+	colour.generateHex();
+	colours.push(colour);
 }
 
-function copyHexToClipBoard(colour){
-    const hexCode = colour.parentElement.parentElement.parentElement.dataset.hexcode;
-    navigator.clipboard.writeText(hexCode);
-}
 
+document.querySelector(".start-button").addEventListener("click", () => {
+	for (let i = 0; i < colours.length; i++) {
+		colours[i].generateHex();
+	}
+});
 
-// Generates a new colour palette as soon as page is loaded.
-document.addEventListener("DOMContentLoaded", generateColourPalette);
-
-// Generates a new colour palette when button is pressed
-const startButton = document.querySelector(".start-button");
-startButton.addEventListener("click", generateColourPalette)
-
-// Generates a new colour palette when user hits spacebar.
 document.addEventListener("keydown", (e) => {
-    if (e.code.toLowerCase() === "space") {
-      for (let i = 0; i < colours.length; i++) {
-        colours[i].generateHex();
-      }
+  if (e.code.toLowerCase() === "space") {
+    for (let i = 0; i < colours.length; i++) {
+      colours[i].generateHex();
     }
-  });
+  }
+})
+
+
